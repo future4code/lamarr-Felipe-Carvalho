cf534f752cb7164e35b3bc737b62daecb44f2a7e
Felipe Carvalho <felipecarvalho.email@gmail.com>
2022-05-04 17:41:22 -0300

    Exercícios laços_vs02

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -19,60 +19,60 @@
 
 // a)
 
-// function textoA(texto){
-//      console.log(texto)  
-//  }
+function textoA(texto){
+     console.log(texto)  
+ }
 
-//  textoA("Eu sou Felipe, tenho 42 anos, moro em BH e sou estudante")
+ textoA("Eu sou Felipe, tenho 42 anos, moro em BH e sou estudante")
 
 // b)
 
-// let nomeUser = prompt("Qual seu nome?")
-// let idadeUser = prompt("Qual sua idade?")
-// let enderecoUser = prompt("Qual seu endereço?")
-// let profissaoUser = prompt("Qual sua profissão?")
+let nomeUser = prompt("Qual seu nome?")
+let idadeUser = prompt("Qual sua idade?")
+let enderecoUser = prompt("Qual seu endereço?")
+let profissaoUser = prompt("Qual sua profissão?")
 
-// function textoB(nome, idade, endereco,profissao) {
-//     console.log("Eu sou "+nome+", tenho "+idade+" anos, moro na rua "+endereco+" e sou "+profissao+".")
-// }
+function textoB(nome, idade, endereco,profissao) {
+    console.log("Eu sou "+nome+", tenho "+idade+" anos, moro na rua "+endereco+" e sou "+profissao+".")
+}
 
-// textoB(nomeUser, idadeUser, enderecoUser, profissaoUser)
+textoB(nomeUser, idadeUser, enderecoUser, profissaoUser)
 
 // EXERCÍCIO 2
 
 // a)
 
-// let numero1 = Number(prompt("Insira um número"))
-// let numero2 = Number(prompt("Insira um outro número"))
+let numero1 = Number(prompt("Insira um número"))
+let numero2 = Number(prompt("Insira um outro número"))
 
-// function somaNum(num1, num2) {
-//     return num1 + num2
-// }
-// console.log(somaNum(numero1, numero2))
+function somaNum(num1, num2) {
+    return num1 + num2
+}
+console.log(somaNum(numero1, numero2))
 
 // b)
 
-// function boleana1(num1, num2){
-//  return num1 > num2
-// }
-// console.log(boleana1(30, 20))
+function boleana1(num1, num2){
+ return num1 > num2
+}
+console.log(boleana1(30, 20))
 
 // c)
 
-// function boleana2(num1){
-//     let restDiv = num1 % 2
-//     return restDiv === 0
-// }
-// console.log(boleana2(35))
+function boleana2(num1){
+    let restDiv = num1 % 2
+    return restDiv === 0
+}
+console.log(boleana2(35))
 
 // d)
 
-// function mensagem(textoMensagem){
-//     const tamFrase = textoMensagem.length
-//     const fraseMaiusc = textoMensagem.toUpperCase()
-//     console.log(tamFrase, fraseMaiusc)
-// }
-// mensagem("Esta é a mensagem")
+function mensagem(textoMensagem){
+    const tamFrase = textoMensagem.length
+    const fraseMaiusc = textoMensagem.toUpperCase()
+    console.log(tamFrase, fraseMaiusc)
+}
+mensagem("Esta é a mensagem")
 
 // EXERCÍCIO 3
 
