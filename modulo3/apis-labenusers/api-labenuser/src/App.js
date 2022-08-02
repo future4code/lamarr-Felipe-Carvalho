a2625617d5cf122fbc998fdd46cd3b05e88c147c
Felipe Carvalho <felipecarvalho.email@gmail.com>
2022-08-02 15:20:35 -0300

    astromatch-01

---
+++ modulo3/apis-labenusers/api-labenuser/src/App.js
@@ -0,0 +1,14 @@
+import React  from "react"
+import  Home  from "./components/page1/pageHome"
+import  Users  from "./components/page2/pageUsers"
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
